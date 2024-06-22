0d8f57f61b296bb40fbd93d4292cbb9c32158a5d
Heera <92657209+mykuromi@users.noreply.github.com>
2024-06-22 15:25:07 +0900

    (study) useMemo

---
+++ src/components/BuseMemo.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useMemo, useState } from "react";
+
+const ExpensiveComponent = ({ value }) => {
+  useEffect(() => {
+    console.log("useMemo - rendering");
+  });
+
+  return <span>{value + 1000}</span>;
+};
+
+export const BuseMemo = () => {
+  const [value, setValue] = useState(10);
+
+  // useMemo value가 변하지 않는 이상 렌더링 X
+  const [, triggerRendering] = useState(false);
+
+  const MemoizedComponent = useMemo(
+    () => <ExpensiveComponent value={value} />,
+    [value]
+  );
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  function handleClick() {
+    triggerRendering((prev) => !prev);
+  }
+
+  return (
+    <div>
+      <input value={value} onChange={handleChange} />
+      <button onClick={handleClick}>렌더링 발생</button>
+      <br />
+      MemoziedComponent : {MemoizedComponent}
+    </div>
+  );
+};
